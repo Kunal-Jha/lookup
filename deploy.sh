3459cb2e225df9f07cc4d0f39967506ba51d06d0
Kunal <kunal94jha@gmail.com>
2016-06-27 14:52:36 +0200

    Deploy with encrypted key

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,12 @@
-#!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
 
-SOURCE_BRANCH="V2_improvements"
-TARGET_BRANCH="gh-pages"
-
-function doCompile {
-  ./compile.sh
-}
+SOURCE_BRANCH="master"
+TARGET_BRANCH="V2_improvements"
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
 if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
     echo "Skipping deploy; just doing a build."
-    doCompile
+    npm run spec
     exit 0
 fi
 
@@ -22,8 +17,9 @@ SHA=`git rev-parse --verify HEAD`
 
 # Clone the existing gh-pages for this repo into out/
 # Create a new empty branch if gh-pages doesn't exist yet (should only happen on first deply)
-git clone $REPO out
-cd out
+git clone $REPO target
+cd target
+cd generatedswaggerdoc
 git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
 cd ..
 
@@ -31,16 +27,17 @@ cd ..
 rm -rf out/**/* || exit 0
 
 # Run our compile script
-doCompile
+npm run spec
 
 # Now let's go have some fun with the cloned repo
-cd out
-git config user.name "Travis CI"
-git config user.email "$COMMIT_AUTHOR_EMAIL"
+cd target
+cd generatedswaggerdoc
+git config user.name "Kunal-Jha"
+git config user.email "kunal94jha@gmail.com"
 
-# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
+# If there are no changes (e.g. this is a README update) then just bail.
 if [ -z `git diff --exit-code` ]; then
-    echo "No changes to the output on this push; exiting."
+    echo "No changes to the spec on this push; exiting."
     exit 0
 fi
 
